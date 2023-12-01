05a5b4d5dbc40113399ebfea9cf9c88714330cda
kwik123 <kwik123@users.noreply.github.com>
2023-12-01 16:14:11 +0100

    added broken filtersong query

--- backend/src/main/java/me/paultje52/fontysChallenge/guldenSchijf/controller/SongController.java
+++ backend/src/main/java/me/paultje52/fontysChallenge/guldenSchijf/controller/SongController.java
@@ -2,16 +2,11 @@ package me.paultje52.fontysChallenge.guldenSchijf.controller;
 
 import me.paultje52.fontysChallenge.guldenSchijf.model.SongModel;
 import me.paultje52.fontysChallenge.guldenSchijf.service.SongService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @RestController
@@ -20,12 +15,20 @@ import java.io.IOException;
 public class SongController {
     @Autowired
     private SongService songService;
-    @RequestMapping("")
-    public SongModel[] getAllSongs(){
+
+    @GetMapping("")
+    public SongModel[] getAllSongs() {
         return songService.GetAllSongs();
     }
 
-    @RequestMapping("/id/{id}")
+    @RequestMapping("/search")
+    public SongModel[] searchAllSongs(@RequestParam(required = false) String title,@RequestParam(required = false) String band,@RequestParam(required = false) String manager,@RequestParam(required = false) String supplier){
+
+        return songService.GetAllSongsFiltered(title,band,manager,supplier);
+
+    }
+
+    @RequestMapping("/{id}.mp3")
     public ResponseEntity<InputStreamResource> songById(@PathVariable int id) throws IOException {
         return songService.Mp3(id);
     }
--- backend/src/main/java/me/paultje52/fontysChallenge/guldenSchijf/repository/SongRepository.java
+++ backend/src/main/java/me/paultje52/fontysChallenge/guldenSchijf/repository/SongRepository.java
@@ -1,14 +1,16 @@
 package me.paultje52.fontysChallenge.guldenSchijf.repository;
 
+import jdk.jfr.Name;
 import me.paultje52.fontysChallenge.guldenSchijf.model.SongModel;
 import me.paultje52.fontysChallenge.guldenSchijf.model.UserModel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SongRepository extends CrudRepository<SongModel, Integer> {
 
-    @Query(value = "SELECT * FROM song INNER JOIN band on song.bandId = band.id inner join manager on song.managerId = manager.id inner join supplier on song.supplierId = supplier.id", nativeQuery = true)
-    Iterable<SongModel> getAllSongs();
+    @Query(value = "SELECT song.id,title,year,duration, filePath, bandId, managerId, supplierId FROM song inner join band on song.bandId = band.id inner join manager on song.managerId = manager.id inner join supplier on song.supplierId = supplier.id where title = %?1% AND band.name = %?2%", nativeQuery = true)
+    Iterable<SongModel> getAllSongsByFilterd(@Param("title") String title, String band,String manager,String supplier);
 }
